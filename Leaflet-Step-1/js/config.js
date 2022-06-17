538529d69155d5032702c38ff21650eda63cd273
Maica Tran <98513893+MAICATRAN@users.noreply.github.com>
2022-06-17 23:30:19 +1000

    Add files via upload

---
+++ Leaflet-Step-1/js/config.js
@@ -0,0 +1,6 @@
+// API key
+
+const MAP_URL1 = "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}"
+const API_KEY1 ="pk.eyJ1IjoibWFpY2F0cmFuIiwiYSI6ImNsNGIyYTd2bzBpeXgzaW4ycXVmaWhwcnEifQ.FZVFJp6zvjn8RZr3_ajZEA"
+
+
---
+++ Leaflet-Step-2/js/config.js
@@ -0,0 +1,2 @@
+const MAP_URL2= "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}"
+const API_KEY2 ="pk.eyJ1IjoibWFpY2F0cmFuIiwiYSI6ImNsNGIyYTd2bzBpeXgzaW4ycXVmaWhwcnEifQ.FZVFJp6zvjn8RZr3_ajZEA"
